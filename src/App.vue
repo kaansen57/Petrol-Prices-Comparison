d7ea87faeb05f6ae72a3d11803831dff9762600b
Kaan SEN <kaansen57@outlook.com>
2020-12-30 00:01:16 +0300

    city options added.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,24 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div class="container-fluid">
+    <Home />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import Home from "@/views/Home";
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    Home,
+  },
+};
+</script>
+<style scoped>
+.container-fluid{
+  padding:0 !important;
 }
 
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,81 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Header />
+    <div class="row">
+      <div class="col-md-5 custom-margin">
+        <div class="input-group mb-3 px-3">
+          <select
+            class="custom-select"
+            id="inputGroupSelect01"
+            @change="ilId($event)"
+            v-model="cityId"
+          >
+            <option selected>Seçiniz</option>
+            <option :value="i" v-for="(il, i) in iller" :key="i">
+              {{ il.il }}
+            </option>
+          </select>
+          
+          <select class="custom-select" id="inputGroupSelect01">
+            <option v-for="(ilce, i) in ilceler" :key="i">{{ ilce }}</option>
+          </select>
+        </div>
+      </div>
+
+      <div class="col-md-2 text-center custom-margin">
+        <button type="button" class="btn btn-danger" @click="bas">
+          Comparison
+        </button>
+      </div>
+
+      <div class="col-md-5 custom-margin">
+      
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Header from "@/components/Header";
+import cityJSON from "@/il-ilce.json";
 export default {
-  name: 'Home',
+  data() {
+    return {
+      iller: [],
+      cityId: "",
+      cityNo: 1,
+      ilceler: [],
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    Header,
+  },
+  methods: {
+    ilId(event) {
+      this.cityNo = parseInt(event.target.value);
+    },
+    bas() {
+      // console.log(this.iller[this.cityId].ilceleri);
+    },
+  },
+  watch: {
+    cityNo(yeni, eski) {
+      this.ilceler = [];
+      for (let i of this.iller[yeni].ilceleri) {
+        this.ilceler.push(i);
+      }
+      console.log(this.ilceler);
+    },
+  },
+  created() {
+    this.iller = cityJSON;
+   
+  },
+};
 </script>
+
+<style scoped>
+.custom-margin {
+  margin-top: 8rem;
+}
+</style>
